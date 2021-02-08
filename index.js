dba2bce8ca164208c04d435b2806547e32fe84cf
Robert Stevens <Robert@21dle.co.uk>
2021-02-08 12:50:47 +0000

    port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var socketIO = require('socket.io');
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8053);
+}).listen(8080);
 
 var io = socketIO.listen(app);
 
